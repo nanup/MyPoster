fbd1b98f7bb1e8a47c51e4ed027faa6509514e8d
nanup <nanup.personal+github@gmail.com>
2023-02-01 15:31:34 +0530

    add react router, pages: users, newposter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Users from './user/pages/Users';
+import NewPoster from "./posters/pages/NewPoster";
 
-function App() {
-  return <h1>Let's start!</h1>;
+function App () {
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Users />} />
+      <Route path="/posters/new" element={<NewPoster />} />
+      <Route path="/*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </Router>
 }
 
 export default App;
---
+++ frontend/src/posters/pages/NewPoster.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPoster = () => {
+  return <h2>New Poster</h2>
+}
+
+export default NewPoster;
