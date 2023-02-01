22f5351325c749532ee78e34aa334d50258b5226
nanup <nanup.personal+github@gmail.com>
2023-02-01 21:56:32 +0530

    add sidebar

---
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./SideDrawer.css";
+
+const SideDrawer = (props) => {
+  return <aside className="side-drawer">
+    {props.children}
+  </aside>
+}
+
+export default SideDrawer;
