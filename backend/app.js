809259eff3fc60f5d3e367f915f3a5369b780584
nanup <nanup.personal+github@gmail.com>
2023-03-01 17:20:03 +0530

    allow some origin, headers and methods

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+})
+
 app.use("/api/posters", posterRoutes);
 app.use("/api/users", userRoutes);
 
