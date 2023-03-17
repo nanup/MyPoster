caaec04c358c2df68f0c01605d3c266604e81dac
nanup <nanup.personal+github@gmail.com>
2023-03-17 12:25:25 +0530

    small fix

--- backend/middleware/check-token.js
+++ backend/middleware/check-token.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
     if (!token) {
       throw new Error("Authentication failed!");
     } else {
-      const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
+      const decodedToken = jwt.verify(token, process.env.JWT_KEY);
       req.userData = { userId: decodedToken.userId };
       next();
     }
