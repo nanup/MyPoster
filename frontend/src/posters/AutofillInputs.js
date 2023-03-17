4b66d7cf9e4c9bd368ff94f1cc0d0147bfe9e729
nanup <nanup.personal+github@gmail.com>
2023-03-17 23:58:51 +0530

    refactors autofill data

---
+++ frontend/src/posters/AutofillInputs.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    title: "Everything Everywhere All at Once",
+    year: 2022,
+    image:
+      "https://www.themoviedb.org/t/p/original/v1zlUrRdMZ2r8zQAbkFjXGug96O.jpg",
+    trailerLink: "https://www.youtube.com/embed/wxN1T1uxQ2g",
+    description:
+      "An aging Chinese immigrant is swept up in an insane adventure, where she alone can save what's important to her by connecting with the lives she could have led in other universes.",
+  },
+  {
+    title: "Avatar: The Way of Water",
+    year: 2022,
+    image:
+      "https://www.themoviedb.org/t/p/original/buGUFtNDeduNQk0Fk2iWo0kgUI1.jpg",
+    trailerLink: "https://www.youtube.com/embed/a8Gx8wiNbs8",
+    description:
+      "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+  },
+  {
+    title: "Top Gun: Maverick",
+    year: 2022,
+    image:
+      "https://www.themoviedb.org/t/p/original/zOGINv5sJxEZQWw2dGuO8JUzvyK.jpg",
+    trailerLink: "https://www.youtube.com/embed/qSqVVswa420",
+    description:
+      "After more than thirty years of service as one of the Navy’s top aviators, and dodging the advancement in rank that would ground him, Pete “Maverick” Mitchell finds himself training a detachment of TOP GUN graduates for a specialized mission the likes of which no living pilot has ever seen.",
+  },
+  {
+    title: "The Last of Us",
+    year: 2022,
+    image:
+      "https://www.themoviedb.org/t/p/original/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg",
+    trailerLink: "https://www.youtube.com/embed/EVyDWbsucRA",
+    description:
+      "Twenty years after modern civilization has been destroyed, Joel, a hardened survivor, is hired to smuggle Ellie, a 14-year-old girl, out of an oppressive quarantine zone. What starts as a small job soon becomes a brutal, heartbreaking journey, as they both must traverse the United States and depend on each other for survival.",
+  },
+  {
+    title: "Chainsaw Man",
+    year: 2022,
+    image:
+      "https://www.themoviedb.org/t/p/original/npdB6eFzizki0WaZ1OvKcJrWe97.jpg",
+    trailerLink: "https://www.youtube.com/embed/5Trz4N2bNy8",
+    description:
+      "Denji has a simple dream—to live a happy and peaceful life, spending time with a girl he likes. This is a far cry from reality, however, as Denji is forced by the yakuza into killing devils in order to pay off his crushing debts. Using his pet devil Pochita as a weapon, he is ready to do anything for a bit of cash.",
+  },
+  {
+    title: "Puss in Boots: The Last Wish",
+    year: 2022,
+    image:
+      "https://www.themoviedb.org/t/p/original/bE5dsNfmlClOfKqI121jqO5kTTw.jpg",
+    trailerLink: "https://www.youtube.com/embed/xgZLXyqbYOc",
+    description:
+      "Puss in Boots discovers that his passion for adventure has taken its toll: He has burned through eight of his nine lives, leaving him with only one life left. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives.",
+  },
+  {
+    title: "All Quiet on the Western Front",
+    year: 2022,
+    image:
+      "https://www.themoviedb.org/t/p/original/pyWyCo0KE1HpwxaCpeMI3qjUYBl.jpg",
+    trailerLink: "https://www.youtube.com/embed/qFqgmaO15x4",
+    description:
+      "Paul Baumer and his friends Albert and Muller, egged on by romantic dreams of heroism, voluntarily enlist in the German army. Full of excitement and patriotic fervour, the boys enthusiastically march into a war they believe in. But once on the Western Front, they discover the soul-destroying horror of World War I.",
+  },
+  {
+    title: "John Wick: Chapter 4",
+    year: 2023,
+    image:
+      "https://www.themoviedb.org/t/p/original/iXc1UMajpm0ZmDWN5ldpkIb8Pib.jpg",
+    trailerLink: "https://www.youtube.com/embed/yjRHZEUamCc",
+    description:
+      "With the price on his head ever increasing, John Wick uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.",
+  },
+  {
+    title: "Pokémon",
+    year: 1997,
+    image:
+      "https://www.themoviedb.org/t/p/original/mYJ4rQlerKZDoBMeTgJIDtdou5H.jpg",
+    trailerLink: "https://www.youtube.com/embed/hxooaOBJUjs",
+    description:
+      "Join Ash accompanied by his partner Pikachu, as he travels through many regions, meets new friends and faces new challenges on his quest to become a Pokémon Master.",
+  },
+  {
+    title: "The Mandalorian",
+    year: 2019,
+    image:
+      "https://www.themoviedb.org/t/p/original/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg",
+    trailerLink: "https://www.youtube.com/embed/aOC8E8z_ifw",
+    description:
+      "After the fall of the Galactic Empire, lawlessness has spread throughout the galaxy. A lone gunfighter makes his way through the outer reaches, earning his keep as a bounty hunter.",
+  },
+];
--- frontend/src/posters/pages/NewPoster.js
+++ frontend/src/posters/pages/NewPoster.js
@@ -6,6 +6,7 @@ import {
   VALIDATOR_REQUIRE,
 } from "../../shared/components/util/validators";
 
+import AUTOFILL_INPUTS from "../AutofillInputs";
 import { AuthContext } from "../../shared/components/FormElements/context/auth-context";
 import Button from "./../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
@@ -16,54 +17,6 @@ import { useContext } from "react";
 import useForm from "./../../shared/hooks/form-hook";
 import { useHttpClient } from "./../../shared/hooks/http-hook";
 import { useNavigate } from "react-router-dom";
-
-const AUTOFILL_INPUTS = [
-  {
-    title: "Everything Everywhere All at Once",
-    year: 2022,
-    image:
-      "https://www.themoviedb.org/t/p/original/v1zlUrRdMZ2r8zQAbkFjXGug96O.jpg",
-    trailerLink: "https://www.youtube.com/embed/wxN1T1uxQ2g",
-    description:
-      "An aging Chinese immigrant is swept up in an insane adventure, where she alone can save what's important to her by connecting with the lives she could have led in other universes.",
-  },
-  {
-    title: "Avatar: The Way of Water",
-    year: 2022,
-    image:
-      "https://www.themoviedb.org/t/p/original/buGUFtNDeduNQk0Fk2iWo0kgUI1.jpg",
-    trailerLink: "https://www.youtube.com/embed/a8Gx8wiNbs8",
-    description:
-      "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-  },
-  {
-    title: "Top Gun: Maverick",
-    year: 2022,
-    image:
-      "https://www.themoviedb.org/t/p/original/zOGINv5sJxEZQWw2dGuO8JUzvyK.jpg",
-    trailerLink: "https://www.youtube.com/embed/qSqVVswa420",
-    description:
-      "After more than thirty years of service as one of the Navy’s top aviators, and dodging the advancement in rank that would ground him, Pete “Maverick” Mitchell finds himself training a detachment of TOP GUN graduates for a specialized mission the likes of which no living pilot has ever seen.",
-  },
-  {
-    title: "The Last of Us",
-    year: 2022,
-    image:
-      "https://www.themoviedb.org/t/p/original/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg",
-    trailerLink: "https://www.youtube.com/embed/EVyDWbsucRA",
-    description:
-      "Twenty years after modern civilization has been destroyed, Joel, a hardened survivor, is hired to smuggle Ellie, a 14-year-old girl, out of an oppressive quarantine zone. What starts as a small job soon becomes a brutal, heartbreaking journey, as they both must traverse the United States and depend on each other for survival.",
-  },
-  {
-    title: "Chainsaw Man",
-    year: 2022,
-    image:
-      "https://www.themoviedb.org/t/p/original/npdB6eFzizki0WaZ1OvKcJrWe97.jpg",
-    trailerLink: "https://www.youtube.com/embed/5Trz4N2bNy8",
-    description:
-      "Denji has a simple dream—to live a happy and peaceful life, spending time with a girl he likes. This is a far cry from reality, however, as Denji is forced by the yakuza into killing devils in order to pay off his crushing debts. Using his pet devil Pochita as a weapon, he is ready to do anything for a bit of cash.",
-  },
-];
 
 const NewPoster = () => {
   const ctx = useContext(AuthContext);
