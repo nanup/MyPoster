e57772dd609345374e76ab8f7c7f59498217c915
nanup <nanup.personal+github@gmail.com>
2023-05-01 09:56:52 +0530

    feat: save

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,7 @@ app.use((error, req, res, next) => {
   }
 });
 
+// Server
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mdhqaux.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
