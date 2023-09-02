cf7b8ec9b2bd053323772c9e811aece6150f7400
nanup <nanup.personal+github@gmail.com>
2023-09-02 12:42:57 +0530

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,16 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type',
+    'Authorization',
+    'Origin'
+  );
   next();
 });
 
